c6a3a43b07af60d23d3dea121c767cb7595bc2f5
Alexey Zotov <alexey1.zotov@yandex.ru>
2026-04-06 18:11:25 +0300

    feat:(vessel) - add logic to calculate LCF directon manual or auto

--- internal/vessel/vessel.go
+++ internal/vessel/vessel.go
@@ -23,28 +23,29 @@ const (
 )
 
 type VesselData struct {
-	Name             string           `json:"name"`
-	Flag             string           `json:"flag"`
-	IMO              string           `json:"imo"`
-	BuiltYear        int              `json:"built_year"`
-	Lightship        float64          `json:"lightship"` // вес порожнем, MT
-	Breadth          float64          `json:"breadth"`   // ширина корпуса, м
-	Depth            float64          `json:"depth"`     // высота корпуса, м
-	LBP              float64          `json:"lbp"`       // длина между перпендикулярами, м
-	SummerDraft      float64          `json:"summer_draft"`
-	SummerDWT        float64          `json:"summer_dwt"`
-	SummerTPC        float64          `json:"summer_tpc"`
-	SummerFreeboard  float64          `json:"summer_freeboard"`
-	DistancePPFwd    float64          `json:"distance_pp_fwd"`
-	PPFwdDirection   PPDirection      `json:"pp_fwd_direction"`
-	DistancePPMid    float64          `json:"distance_pp_mid"`
-	PPMidDirection   PPDirection      `json:"pp_mid_direction"`
-	DistancePPAft    float64          `json:"distance_pp_aft"`
-	PPAftDirection   PPDirection      `json:"pp_aft_direction"`
-	KeelFwd          float64          `json:"keel_fwd"`
-	KeelMid          float64          `json:"keel_mid"`
-	KeelAft          float64          `json:"keel_aft"`
-	VesselType       VesselType       `json:"vessel_type"`
-	CorrectionMethod CorrectionMethod `json:"correction_method"`
-	TableDensity     *float64         `json:"table_density"`
+	Name                 string           `json:"name"`
+	Flag                 string           `json:"flag"`
+	IMO                  string           `json:"imo"`
+	BuiltYear            int              `json:"built_year"`
+	Lightship            float64          `json:"lightship"` // вес порожнем, MT
+	Breadth              float64          `json:"breadth"`   // ширина корпуса, м
+	Depth                float64          `json:"depth"`     // высота корпуса, м
+	LBP                  float64          `json:"lbp"`       // длина между перпендикулярами, м
+	SummerDraft          float64          `json:"summer_draft"`
+	SummerDWT            float64          `json:"summer_dwt"`
+	SummerTPC            float64          `json:"summer_tpc"`
+	SummerFreeboard      float64          `json:"summer_freeboard"`
+	DistancePPFwd        float64          `json:"distance_pp_fwd"`
+	PPFwdDirection       PPDirection      `json:"pp_fwd_direction"`
+	DistancePPMid        float64          `json:"distance_pp_mid"`
+	PPMidDirection       PPDirection      `json:"pp_mid_direction"`
+	DistancePPAft        float64          `json:"distance_pp_aft"`
+	PPAftDirection       PPDirection      `json:"pp_aft_direction"`
+	KeelFwd              float64          `json:"keel_fwd"`
+	KeelMid              float64          `json:"keel_mid"`
+	KeelAft              float64          `json:"keel_aft"`
+	VesselType           VesselType       `json:"vessel_type"`
+	CorrectionMethod     CorrectionMethod `json:"correction_method"`
+	IsLcfDetectionManual bool             `json:"is_lcf_detection_manual"`
+	TableDensity         *float64         `json:"table_density"`
 }
