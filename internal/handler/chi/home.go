aff1e29ec57855bc30cd10cef6b1271c01e5f397
Alexey Zotov <alexey1.zotov@yandex.ru>
2026-05-27 22:52:24 +0300

    feat(chi): add home handler with explicit user nil check and redirect

--- internal/handler/chi/home.go
+++ internal/handler/chi/home.go
@@ -1,23 +1,28 @@
 package chi
 
 import (
-	"errors"
 	"net/http"
-	"os"
 
-	"github.com/AVZotov/draft-survey/internal/handler/tadaptor"
 	"github.com/AVZotov/draft-survey/web"
 	"github.com/AVZotov/draft-survey/web/templates/pages"
-	"github.com/gofiber/fiber/v2"
 )
 
-func (h *Handler) home(c *fiber.Ctx) error {
+func (h *Handler) home(w http.ResponseWriter, r *http.Request) {
+	const op = "home"
 	user, err := h.userRepository.Get()
-	if err != nil && !errors.Is(err, os.ErrNotExist) {
-		return err
+	if err != nil {
+		h.logger.Error(op, err)
+		//TODO: Implement error handling. Avoid handling no user error
+	}
+	if user == nil {
+		http.Redirect(w, r, "/profile", http.StatusSeeOther)
+		return
 	}
 
 	props := web.DashboardLayoutProps(user, h.appVersion)
-	c.Status(http.StatusOK)
-	return tadaptor.Render(c, pages.Dashboard(props))
+	err = pages.Dashboard(props).Render(r.Context(), w)
+	if err != nil {
+		h.logger.Error(op, err)
+		//TODO: Implement error handling.
+	}
 }
