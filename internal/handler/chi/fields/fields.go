e29bbaf4495758c2996ccb46c333ecea292be1a7
Alexey Zotov <alexey1.zotov@yandex.ru>
2026-05-28 15:17:49 +0300

    feat(chi): add form field constants for survey, vessel and settings blocks

--- internal/handler/chi/fields/fields.go
+++ internal/handler/chi/fields/fields.go
@@ -12,3 +12,44 @@ const (
 	FieldEmployeeID = "employee_id"
 	FieldSignature  = "signature"
 )
+
+// Survey Block
+const (
+	FieldJobNumber       = "job_number"
+	FieldDSNumber        = "ds_number"
+	FieldCargoOperation  = "cargo_operation"
+	FieldCargo           = "cargo"
+	FieldPacking         = "packing"
+	FieldClient          = "client"
+	FieldCargoDeclared   = "cargo_declared"
+	FieldRemarks         = "remarks"
+	FieldLoadCountryCode = "load_country_code"
+	FieldLoadCountryName = "load_country_name"
+)
+
+// Vessel Block
+const (
+	FieldVesselName      = "vessel_name"
+	FieldIMO             = "imo"
+	FieldBuiltYear       = "built_year"
+	FieldConstDeclared   = "const_declared"
+	FieldTableDensity    = "table_density"
+	FieldLightship       = "lightship"
+	FieldLBP             = "lbp"
+	FieldBreadth         = "breadth"
+	FieldDepth           = "depth"
+	FieldSummerDraft     = "summer_draft"
+	FieldSummerDWT       = "summer_dwt"
+	FieldSummerTPC       = "summer_tpc"
+	FieldSummerFreeboard = "summer_freeboard"
+	FieldFlagCountryCode = "flag_country_code"
+	FieldFlagCountryName = "flag_country_name"
+	FieldHoldsTotal      = "holds_total"
+)
+
+// Settings Block
+const (
+	FieldMMCMethod    = "mmc_method"
+	FieldCorrMethod   = "corr_method"
+	FieldLCFDetection = "lcf_detection"
+)
