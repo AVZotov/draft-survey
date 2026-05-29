c20bce73e93ba81e5e05aa543ff3a9a0f4d4b408
Alexey Zotov <alexey1.zotov@yandex.ru>
2026-05-29 18:01:40 +0300

    feat(chi): add draft block parsers (Marks, PPKeel, Hydrostatics, Deductibles, SeaCondition)

--- internal/handler/chi/fields/fields.go
+++ internal/handler/chi/fields/fields.go
@@ -1,5 +1,13 @@
 package fields
 
+import "fmt"
+
+// WithIndex helper function
+// for consistent naming of fields received from HTML requests
+func WithIndex(field string, index int) string {
+	return fmt.Sprintf("%s-%d", field, index)
+}
+
 // Profile form fields
 const (
 	FieldFirstName  = "first_name"
@@ -52,4 +60,83 @@ const (
 	FieldMMCMethod    = "mmc_method"
 	FieldCorrMethod   = "corr_method"
 	FieldLCFDetection = "lcf_detection"
+)
+
+// Draft Info Block
+const (
+	FieldLoadPort      = "load_port"
+	FieldDischargePort = "discharge_port"
+	FieldProductGrade  = "product_grade"
+)
+
+// Sea Condition Block
+const (
+	FieldSeaType      = "sea_type"
+	FieldSeaCondition = "sea_condition"
+)
+
+// FieldHoldsActive Holds Block
+const (
+	FieldHoldsActive = "holds_active"
+)
+
+// Marks Block
+const (
+	FieldFwdPort       = "fwd_port"
+	FieldFwdPortMethod = "fwd_port_method"
+	FieldFwdStbd       = "fwd_stbd"
+	FieldFwdStbdMethod = "fwd_stbd_method"
+	FieldMidPort       = "mid_port"
+	FieldMidPortMethod = "mid_port_method"
+	FieldMidStbd       = "mid_stbd"
+	FieldMidStbdMethod = "mid_stbd_method"
+	FieldAftPort       = "aft_port"
+	FieldAftPortMethod = "aft_port_method"
+	FieldAftStbd       = "aft_stbd"
+	FieldAftStbdMethod = "aft_stbd_method"
+	FieldTPCListPort   = "tpc_list_port"
+	FieldTPCListStbd   = "tpc_list_stbd"
+)
+
+// PP Keel Block
+const (
+	FieldDFwd    = "d_fwd"
+	FieldDFwdDir = "d_fwd_dir"
+	FieldDMid    = "d_mid"
+	FieldDMidDir = "d_mid_dir"
+	FieldDAft    = "d_aft"
+	FieldDAftDir = "d_aft_dir"
+	FieldKeelFwd = "keel_fwd"
+	FieldKeelMid = "keel_mid"
+	FieldKeelAft = "keel_aft"
+)
+
+// Hydrostatics Block
+const (
+	FieldUDraft    = "u_draft"
+	FieldUDisp     = "u_disp"
+	FieldUTPC      = "u_tpc"
+	FieldULCF      = "u_lcf"
+	FieldULCFDir   = "u_lcf_dir"
+	FieldLDraft    = "l_draft"
+	FieldLDisp     = "l_disp"
+	FieldLTPC      = "l_tpc"
+	FieldLLCF      = "l_lcf"
+	FieldLLCFDir   = "l_lcf_dir"
+	FieldPMTCDraft = "p_mtc_draft"
+	FieldPMTC      = "p_mtc"
+	FieldNMTCDraft = "n_mtc_draft"
+	FieldNMTC      = "n_mtc"
+)
+
+// Deductibles Block
+const (
+	FieldHFO              = "hfo"
+	FieldMDO              = "mdo"
+	FieldLubOil           = "lub_oil"
+	FieldBilgeWater       = "bilge_water"
+	FieldDockwaterDensity = "dockwater_density"
+	FieldOthers           = "others"
+	FieldBallastWater     = "ballast_water"
+	FieldFreshWater       = "fresh_water"
 )
